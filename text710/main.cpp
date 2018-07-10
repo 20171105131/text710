6ccefd7f1a6aaec5fc5f05aa27dca8bb57a40454
20171105131 <1105197998@qq.com>
2018-07-10 14:37:28 +0800

    Initial Commit

---
+++ text710/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  text710
+//
+//  Created by 唐伟强 on 2018/7/10.
+//  Copyright © 2018年 唐伟强. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
